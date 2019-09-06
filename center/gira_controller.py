df7f06d0d452c102943a76a4df054ad7d3a7ba63
Daniel Albuschat <d.albuschat@gmail.com>
2019-09-06 22:38:00 +0100

    Made the client handle multiple buttons (required new BLE protocol, the button pin is now encoded in the value, together with the button state)

--- center/gira_controller.py
+++ center/gira_controller.py
@@ -1,35 +1,33 @@
 import requests
 import os
 
-serverIp    = os.getenv("SMARTHOMEREMOTE_SERVERIP")
-dpDim       = os.getenv("SMARTHOMEREMOTE_DP_DIM")
-dpOnOff     = os.getenv("SMARTHOMEREMOTE_DP_ONOFF")
-token       = os.getenv("SMARTHOMEREMOTE_APITOKEN")
-
 class GiraController:
+    def __init__(self, serverIp, token, dpDim, dpOnOff):
+        self.serverIp = serverIp
+        self.token = token
+        self.dpDim = dpDim
+        self.dpOnOff = dpOnOff
+
     def startDim(self, direction):
-        global dpDim, token
         if direction == 1:
             value = 100
         else:
             value = -100
-        requests.put("https://{serverIp}/api/v1/values/{dpDim}?token={token}".format(serverIp=serverIp, dpDim=dpDim, token=token), verify=False, data='{{"value": {}}}'.format(value))
+        requests.put("https://{serverIp}/api/v1/values/{dpDim}?token={token}".format(serverIp=self.serverIp, dpDim=self.dpDim, token=self.token), verify=False, data='{{"value": {}}}'.format(value))
         print("GiraController.startDim({})".format(direction))
 
     def stopDim(self):
-        global dpDim, token
-        requests.put("https://{serverIp}/api/v1/values/{dpDim}?token={token}".format(serverIp=serverIp, dpDim=dpDim, token=token), verify=False, data='{"value": 0}')
+        requests.put("https://{serverIp}/api/v1/values/{dpDim}?token={token}".format(serverIp=self.serverIp, dpDim=self.dpDim, token=self.token), verify=False, data='{"value": 0}')
         print("GiraController.stopDim()")
 
     def toggle(self):
-        global dpOnOff, token
-        r = requests.get("https://{serverIp}/api/v1/values/{dpOnOff}?token={token}".format(serverIp=serverIp, dpOnOff=dpOnOff, token=token), verify=False)
+        r = requests.get("https://{serverIp}/api/v1/values/{dpOnOff}?token={token}".format(serverIp=self.serverIp, dpOnOff=self.dpOnOff, token=self.token), verify=False)
         result = r.json()
         value = 0
         if "values" in result:
             for v in result['values']:
                 if "uid" in v and "value" in v:
-                    if v['uid'] == "a00f":
+                    if v['uid'] == self.dpOnOff:
                         value = int(v['value'])
-        requests.put("https://{serverIp}/api/v1/values/{dpOnOff}?token={token}".format(serverIp=serverIp, dpOnOff=dpOnOff, token=token), verify=False, data='{{"value": {}}}'.format(1 - value))
+        requests.put("https://{serverIp}/api/v1/values/{dpOnOff}?token={token}".format(serverIp=self.serverIp, dpOnOff=self.dpOnOff, token=self.token), verify=False, data='{{"value": {}}}'.format(1 - value))
         print("GiraController.toggle()")
--- center/main.py
+++ center/main.py
@@ -7,23 +7,40 @@ from gira_controller import GiraController
 loop = asyncio.get_event_loop()
 
 CHARACTERISTIC_UUID = '99ca7635-879d-4b08-bca1-0a8ba1ff0d47'
-dimmerHandler = DimmerHandler(GiraController(), AsyncIoScheduler(loop))
+
+scheduler = AsyncIoScheduler(loop)
+
+serverIp    = os.getenv("SMARTHOMEREMOTE_SERVERIP")
+dpDim1      = os.getenv("SMARTHOMEREMOTE_DP_DIM1")
+dpOnOff1    = os.getenv("SMARTHOMEREMOTE_DP_ONOFF1")
+dpDim2      = os.getenv("SMARTHOMEREMOTE_DP_DIM2")
+dpOnOff2    = os.getenv("SMARTHOMEREMOTE_DP_ONOFF2")
+token       = os.getenv("SMARTHOMEREMOTE_APITOKEN")
+ble_name    = os.getenv("SMARTHOMEREMOTE_BLE_SERVERNAME")
+
+dimmerHandler = {
+    22: DimmerHandler(GiraController(serverIp, token, dpDim1, dpOnOff1), scheduler),
+    21: DimmerHandler(GiraController(serverIp, token, dpDim2, dpOnOff2), scheduler)
+}
 
 def notify_receiver(sender, data):
     global dimmerHandler
-    upDown = int.from_bytes(data, byteorder='little')
+    upDown = int.from_bytes(data[1:2], byteorder='little', signed=False)
+    pin = int.from_bytes(data[0:1], byteorder='little', signed=False)
     if upDown == 1:
-        dimmerHandler.button_down()
+        if pin in dimmerHandler:
+           dimmerHandler[pin].button_down()
     else:
-        dimmerHandler.button_up()
+        if pin in dimmerHandler:
+            dimmerHandler[pin].button_up()
     print("Received notification #{}!".format(upDown))
 
 async def run():
-    global CHARACTERISTIC_UUID
+    global CHARACTERISTIC_UUID, ble_name
     devices = await discover()
     for d in devices:
         print('Found {}'.format(d))
-        if d.name == "ESP32" or d.name == os.getenv("SMARTHOMEREMOTE_BLE_SERVERNAME"):
+        if d.name == "ESP32" or d.name == ble_name:
             print("Connecting to {}".format(d))
             async with BleakClient(d.address, loop=loop) as client:
                 print("Connected to {}".format(client))
