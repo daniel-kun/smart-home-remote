e2978abb6f7300f183e096d8e8ef3d450c44c32a
Daniel Albuschat <d.albuschat@gmail.com>
2019-09-08 12:52:12 +0100

    Implemented value caching and live value change notifications in order to be able to quickly toggle on/off

--- center/gira_controller.py
+++ center/gira_controller.py
@@ -1,32 +1,17 @@
-import requests
-import os
-
 class GiraController:
     def __init__(self, giraServie, dpDim, dpOnOff):
         self.giraService = giraServie
         self.dpDim = dpDim
         self.dpOnOff = dpOnOff
 
+        self.giraService.load_cache(self.dpDim)
+        self.giraService.load_cache(self.dpOnOff)
+
     def startDim(self, direction):
-        if direction == 1:
-            value = 100
-        else:
-            value = -100
-        requests.put("https://{serverIp}/api/v1/values/{dpDim}?token={token}".format(serverIp=self.giraService.serverIp, dpDim=self.dpDim, token=self.giraService.token), verify=False, data='{{"value": {}}}'.format(value))
-        print("GiraController.startDim({})".format(direction))
+        self.giraService.startDim(self.dpDim, direction)
 
     def stopDim(self):
-        requests.put("https://{serverIp}/api/v1/values/{dpDim}?token={token}".format(serverIp=self.giraService.serverIp, dpDim=self.dpDim, token=self.giraService.token), verify=False, data='{"value": 0}')
-        print("GiraController.stopDim()")
+        self.giraService.stopDim(self.dpDim)
 
     def toggle(self):
-        r = requests.get("https://{serverIp}/api/v1/values/{dpOnOff}?token={token}".format(serverIp=self.giraService.serverIp, dpOnOff=self.dpOnOff, token=self.giraService.token), verify=False)
-        result = r.json()
-        value = 0
-        if "values" in result:
-            for v in result['values']:
-                if "uid" in v and "value" in v:
-                    if v['uid'] == self.dpOnOff:
-                        value = int(v['value'])
-        requests.put("https://{serverIp}/api/v1/values/{dpOnOff}?token={token}".format(serverIp=self.giraService.serverIp, dpOnOff=self.dpOnOff, token=self.giraService.token), verify=False, data='{{"value": {}}}'.format(1 - value))
-        print("GiraController.toggle()")
+        self.giraService.toggle(self.dpOnOff)
--- center/gira_service.py
+++ center/gira_service.py
@@ -1,17 +1,57 @@
 from aiohttp import web
+import requests
+import json
 
 class GiraService:
     def __init__(self, serverIp, token):
         self.serverIp = serverIp
         self.token = token
+        self.valueCache = dict()
 
-    async def valueChanged(self, request):
+    def load_cache(self, dpUid):
+        if not dpUid in self.valueCache:
+            r = requests.get("https://{serverIp}/api/v1/values/{dpUid}?token={token}".format(serverIp=self.serverIp, dpUid=dpUid, token=self.token), verify=False)
+            j = r.json()
+            if 'values' in j:
+                for v in j['values']:
+                    print("Cached value {} for {}".format(v['value'], v['uid']))
+                    self.valueCache[v['uid']] = v['value']
+
+    def startDim(self, dpDim, direction):
+        if direction == 1:
+            value = 100
+        else:
+            value = -100
+        requests.put("https://{serverIp}/api/v1/values/{dpDim}?token={token}".format(serverIp=self.serverIp, dpDim=dpDim, token=self.token), verify=False, data='{{"value": {}}}'.format(value))
+        print("GiraController.startDim({})".format(direction))
+
+    def stopDim(self, dpDim):
+        requests.put("https://{serverIp}/api/v1/values/{dpDim}?token={token}".format(serverIp=self.serverIp, dpDim=dpDim, token=self.token), verify=False, data='{"value": 0}')
+        print("GiraController.stopDim()")
+
+    def toggle(self, dpOnOff):
+        if dpOnOff in self.valueCache:
+            value = 1 - int(self.valueCache[dpOnOff]) # Invert value
+        else:
+            value = 1 # Just turn on in case we don't know the value, yet
+        requests.put("https://{serverIp}/api/v1/values/{dpOnOff}?token={token}".format(serverIp=self.serverIp, dpOnOff=dpOnOff, token=self.token), verify=False, data='{{"value": {}}}'.format(value))
+        self.valueCache[dpOnOff] = str(value)
+        print("GiraController.toggle()")
+
+    async def value_changed(self, request):
         payload = await request.json()
+        print("Received value_changed: {}".format(json.dumps(payload)))
         if 'events' in payload:
             for e in payload['events']:
                 if 'uid' in e and 'value' in e:
-                    print("UID {} changed to value {}".format(e['uid'], e['value']))
+                    self.valueCache[e['uid']] = e['value']
         return web.Response(text="OK")
 
     def setupWebApp(self, app):
-        app.add_routes([web.post('/value_changed', self.valueChanged)])
+        app.add_routes([web.post('/value_changed', self.value_changed)])
+        r = requests.post(
+            "https://{serverIp}/api/v1/clients/{token}/callbacks".format(serverIp=self.serverIp, token=self.token),
+            verify=False,
+            data='{ "valueCallback": "https://maker-pi:8443/value_changed", "testCallbacks": false }',
+            headers={"Content-Type": "application/json"})
+        print(r)
